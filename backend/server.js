d849617263cc1787b142a337eea42b357bf4c664
npm-sipu <sujit.bhatta95@gmail.com>
2023-04-29 17:34:39 +0530

    added react query and axios

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,19 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import productRouter from "./routes/productRoutes.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+import cors from "cors";
 
 dotenv.config();
 
 connectDb();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("api is running");
